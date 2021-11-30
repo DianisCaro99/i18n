bbd7085e54c74c289cd5ade5037cd27fe0c6fe84
DianisCaro99 <dm.caro@uniandes.edu.co>
2021-11-29 21:33:41 -0500

    Entrega de i18n

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
 
 import JobsList from "./components/jobslist";
+import localeEnMessages from "./locales/en";
+import localeEsMessages from "./locales/es";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+//variabler para cambiar el idioma. en - english y es - español
+const language = 'en';
+const messages = {
+	'es': localeEsMessages,
+	'en': localeEnMessages
+  };
+ReactDOM.render(
+	<IntlProvider locale={language} messages= {messages[language]}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
+console.log(language);
